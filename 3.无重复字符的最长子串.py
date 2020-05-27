a6b0bb9dc12b5f9287e5ab778bbe3937424d947a
kangkang <596286458@qq.com>
2020-05-27 20:37:09 +0800

    字典查询

--- 3.无重复字符的最长子串.py
+++ 3.无重复字符的最长子串.py
@@ -44,17 +44,37 @@
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        max_window = 0
-        a = dict()
-        i = 0
-        j = 0
-        while j < len(s):
-            if s[j] in a:
-                # 保证i靠右
-                i = max(a[s[j]], i)
-            max_window = max(max_window, j-i+1)
-            a[s[j]] = j+1
-            j += 1
+        # max_window = 0
+        # a = dict()
+        # i = 0
+        # j = 0
+        # while j < len(s):
+        #     if s[j] in a:
+        #         # 保证i靠右
+        #         i = max(a[s[j]], i)
+        #     max_window = max(max_window, j-i+1)
+        #     a[s[j]] = j+1
+        #     j += 1
 
-        return max_window
+        # return max_window
+
+        if not s:
+            return 0
+        left = 0
+        lookup = set()
+        n = len(s)
+        max_len = 0
+        cur_len = 0
+        for i in range(n):
+            cur_len += 1
+            while s[i] in lookup:
+                lookup.remove(s[left])
+                left += 1
+                cur_len -= 1
+            if cur_len > max_len:
+                max_len = cur_len
+            lookup.add(s[i])
+        return max_len
+
+
 # @lc code=end
